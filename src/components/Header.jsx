e0d870214fece97e62820216c9c541c12693da14
Blessing Tembo <temboblessing05@gmail.com>
2026-02-07 16:48:23 +0200

    add section in header in lager screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import { Link } from "react-router";
 import "./Header.css";
 import logo from "../assets/images/logo/logo.png";
 
-function Header({varient}) {
+function Header({ varient }) {
   return (
     <header className={`site-header ${varient}-header`}>
       <Link to="/">
-       <img className="logo" src={logo} alt="" />
+        <img className="logo" src={logo} alt="" />
       </Link>
+
+      <nav className="link-section">
+        <Link className="link" to="/plotting">
+          <button className="cali-button">
+            CALIBRATION CURVE
+          </button>
+        </Link>
+      </nav>
 
       <nav className="icon-section">
         <div className="box-icon">
